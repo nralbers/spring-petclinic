d8ad50f380f9210d69f970c1f4a55f42284360bc
Niels Albers <niels.albers@ordina.nl>
2018-02-22 09:50:42 +0100

    Use ServeletInitializer for Tomcat deploys

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,6 +18,8 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * PetClinic Spring Boot Application.
@@ -26,10 +28,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class PetClinicApplication {
+public class PetClinicApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(PetClinicApplication.class, args);
+        SpringApplication.run(applicationClass, args);
     }
+    
+    
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<PetClinicApplication> applicationClass = PetClinicApplication.class;
 
 }
